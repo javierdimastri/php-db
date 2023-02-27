19bc1625e02be178cf1145db0e60abe81ba21d80
hilmantrihatmojo <mohammed.hilmantrihatmojo@btpn.com>
2023-02-27 21:45:20 +0700

    [hilman] add getconnection

---
+++ TestConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$port = 3306;
+$database = "learn_php_db";
+$username = "root";
+$password = "";
+
+
+try {
+    $connection = new PDO("mysql:host=$host:$port;dbname=$database", $username, $password);
+
+    echo "Connect successfully" . PHP_EOL;
+
+    $connection = null;
+}
+catch (PDOException $exception)
+{
+    echo "Failed connected {$exception->getMessage()}";
+}
